989264af229ad51b47df2b0dd6d9f1eae40aef7c
benmcderm <benmcderm@gmail.com>
2016-07-13 19:45:14 -0700

    Working game

---
+++ README.md
@@ -0,0 +1,12 @@
+# Pop
+
+[Pop Game Live][link]
+
+[link]: www.github.com/#
+
+Pop is a browser-based JavaScript game.  The point of the game is to pop the most dots in the time limit.  Dots are popped by dragging across two or more dots of the same color.
+
+
+## Features & Implementation
+
+Built using JavaScript, jQuery, HTML, & CSS.
--- scripts/board.js
+++ scripts/board.js
@@ -2,14 +2,27 @@ const $ = require('jquery');
 const Dot = require('./dot.js');
 const colors = ['red', 'blue', 'yellow', 'green', 'purple'];
 let selectedDots = [];
+let candidates = [];
+
 
 function Board() {
   this.grid = [];
   this.dragging = false;
+  this.score = 0;
 };
 
 function randomColor() {
   return colors[Math.floor(Math.random() * (colors.length - 1))]
+};
+
+Board.prototype.resetBoard = function () {
+  $(`li`).removeAttr('pos');
+  console.log("something was removed");
+  $(`ul`).each(function (i) {
+    $(this).children().each(function(j) {
+      $(this).attr('pos', `${i},${j}`);
+    })
+  });
 };
 
 Board.prototype.populate = function () {
@@ -21,6 +34,7 @@ Board.prototype.populate = function () {
       this.addDot(classColor, [i,j]);
     }
   }
+  $('.score').append(`<div>${this.score}</div>`)
 };
 
 Board.prototype.addDot = function (color, pos) {
@@ -34,7 +48,10 @@ Board.prototype.addDot = function (color, pos) {
 Board.prototype.removeDot = function (pos) {
   let newPos = $(pos).attr('pos');
   this.setDot(undefined, newPos);
-  $(pos).hide('slow', function(){ $(pos).remove(); });
+  $(pos).hide('slow', () =>{
+    $(pos).remove();
+    this.resetBoard();
+  });
 };
 
 Board.prototype.getDot = function(pos) {
@@ -56,8 +73,16 @@ Board.prototype.onStartDragging = function (e) {
 
 Board.prototype.onDotHover = function (e) {
   if (this.dragging) {
-    if (!selectedDots.includes(e.currentTarget)) {
-      selectedDots.push(e.currentTarget);
+    if (!candidates.includes(e.currentTarget)) {
+      candidates.push(e.currentTarget);
+      if (isAdjacentTo(e.currentTarget)) {
+        if (checkForSimilarity(candidates)) {
+          selectedDots.push(e.currentTarget)
+          console.log(candidates);
+        }
+      } else {
+        candidates.pop();
+      }
     }
   }
 };
@@ -71,23 +96,49 @@ function checkForSimilarity (dots) {
   return true;
 };
 
-function isAdjacentTo(pos1, pos2) {
-  
+function isAdjacentTo(dot) {
+  if (candidates.length === 1) {
+    return true;
+  }
+  let adjacent = false;
+  let directions = [[0,1], [1,0], [-1,0], [0,-1]];
+  let dotx = $(dot).attr('pos').slice(0,1);
+  let doty = $(dot).attr('pos').slice(2);
+
+  for (let i = 0; i < candidates.length; i++) {
+
+    let candidatex = $(candidates[i]).attr('pos').slice(0,1)
+    let candidatey = $(candidates[i]).attr('pos').slice(2)
+
+    for (let j = 0; j < directions.length; j++) {
+      if (directions[j][0] === (dotx - candidatex) && directions[j][1] === (doty - candidatey)) {
+        adjacent = true;
+      }
+    }
+  }
+  return adjacent;
 };
 
 Board.prototype.onStopDragging = function () {
   this.dragging = false;
+  if (selectedDots.length < 2) {
+    selectedDots = [];
+    candidates = [];
+    return;
+  }
   if (checkForSimilarity(selectedDots)) {
     selectedDots.forEach((dot) => {
       let columnNumber = $(dot).attr('pos').slice(0,1);
       let rowNumber = 5;
       this.addDot(randomColor(), [columnNumber, rowNumber])
+      this.score += 1;
+      $('.score').text(`${this.score}`)
       this.removeDot(dot);
     });
-    selectedDots = [];
-  } else {
-    selectedDots = [];
   }
+
+  selectedDots = [];
+  candidates = [];
 };
 
 module.exports = Board;
